5d37e25531f72e18dbb965e3f683be43f41da45d
maxamed1st <maxamed1st@icloud.com>
2023-06-13 05:33:12 +0100

    set up firebase for databse and authentication

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+//set up configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBIb18Hzo6JMA8JPw7EWyJK9AxEJ1gogBY",
+  authDomain: "entries-7ee81.firebaseapp.com",
+  projectId: "entries-7ee81",
+  storageBucket: "entries-7ee81.appspot.com",
+  messagingSenderId: "729556529976",
+  appId: "1:729556529976:web:c46de01628d672808947cd",
+  measurementId: "G-S4CSZ7TX8T"
+};
+//initialize firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+//set up emulators
+connectFirestoreEmulator(db, "localhost", 5300);
+connectAuthEmulator(auth, "http://localhost:5500");
