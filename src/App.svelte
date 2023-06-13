199dbcc977a02a6609d05b1d67f52821cdd43de6
maxamed1st <maxamed1st@icloud.com>
2023-06-13 05:35:16 +0100

    expose authentication feature

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,23 @@
-Hello Walaalka!
+<script>
+  import { auth } from "./lib/firebase";
+  import { user } from "./lib/store";
+  import Index from "./features/authentication/Index.svelte";
+  import { signOut } from "firebase/auth";
+function logout() {
+  if($user) {
+    signOut(auth);
+    console.log("user signed out");
+  } else {
+    console.log("No user logged in");
+  }
+}
+</script>
+<main class="grid grid-cols-2 gap-2">
+  <Index />
+  <button on:click={logout} class="w-full mx-auto hover:text-2xl"> logout </button>
+  {#if $user}
+  {$user.email}
+  {:else}
+  no user logged in currently
+  {/if}
+</main>
---
+++ src/features/authentication/Index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import Login from "./components/Login.svelte";
+  import Register from "./components/Register.svelte";
+</script>
+
+<main class="flex [&_*]:flex-1 gap-5">
+  <Register />
+  <Login />
+</main>
