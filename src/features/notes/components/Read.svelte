5733af409703f8040edea86b78c52bf18377011b
maxamed1st <maxamed1st@icloud.com>
2023-06-19 16:04:40 +0100

    view a single note

---
+++ src/features/notes/components/Read.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte";
+  import { userCol } from "../../../lib/store";
+  export let id;
+  let title;
+  let content;
+  onMount(() => {
+    const note = $userCol.find(doc => doc.id == id)
+    title = note.title;
+    content = note.content;
+  })
+</script>
+<article class="flex flex-col gap-2 items-center p-5 m-5">
+  <header>{title}</header>
+  <main>{content}</main>
+</article>
