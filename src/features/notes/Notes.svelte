dc0bd6d245d3a7b723359b18b0edbbf031a0c590
maxamed1st <maxamed1st@icloud.com>
2023-06-19 16:05:24 +0100

    list all notes

---
+++ src/features/notes/Notes.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { userCol } from "../../lib/store";
+  import { auth } from "../../lib/firebase";
+  import { signOut } from "firebase/auth";
+  import { navigate } from "svelte-routing"
+  function readDoc(e) {
+    navigate(`/notes/read/${e.target.id}`);
+  }
+  function createDoc(e) {
+    navigate("/notes/create");
+  }
+</script>
+<main class="flex flex-col gap-2 items-center p-5">
+  <button on:click={e => signOut(auth)} class=" bg-slate-500 hover:bg-slate-400 p-2 rounded"> log out </button>
+{#each $userCol as doc}
+  <button on:click|preventDefault={readDoc} id={doc.id} class="border w-52 h-10 bg-slate-500 hover:bg-slate-400 text-white text-xl">{doc.title}</button>
+{/each}
+<button on:click={createDoc} class="rounded-[110%] text-2xl p-2 bg-slate-500 hover:bg-slate-400"> + </button>
+</main>
