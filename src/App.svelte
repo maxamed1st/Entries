177098f1f0dd842fafc2cbf3226642661fb76850
maxamed1st <maxamed1st@icloud.com>
2023-06-22 18:02:43 +0100

    redirect only at the base url "/"

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,31 @@
 <script>
-  import {Router, navigate, Route} from "svelte-routing";
-  import { user } from "./lib/store";
+  import { Router, navigate, Route } from "svelte-routing";
+  import { user, loading } from "./lib/store";
   import Authentication from "./features/authentication/Authentication.svelte";
   import Notes from "./features/notes/Notes.svelte";
   import Read from "./features/notes/components/Read.svelte";
   import Create from "./features/notes/components/Create.svelte";
   import Folders from "./features/folders/Folders.svelte";
-  import { loading } from "./lib/store";
-  $: if(!$loading) {
-    if($user) navigate("/folders");
+
+  const path = window.location.pathname;
+  $: if (!$loading && path == "/") {
+    if ($user) navigate("/folders");
     else navigate("/authenticate");
   }
 </script>
+
 {#if $loading}
   loading..
 {/if}
 <Router>
-{#if $user}
-<Route path="/notes/read/:id" let:params>
-  <Read id={params.id} />
-</Route>
-<Route path="/notes/create" component={Create} />
-<Route path="/notes" component={Notes} />
-<Route path="/folders" component={Folders} />
-{:else}
-  <Route path="/authenticate" component={Authentication} />
-{/if}
+  {#if $user}
+    <Route path="/notes/read/:id" let:params>
+      <Read id={params.id} />
+    </Route>
+    <Route path="/notes/create" component={Create} />
+    <Route path="/notes" component={Notes} />
+    <Route path="/folders" component={Folders} />
+  {:else}
+    <Route path="/authenticate" component={Authentication} />
+  {/if}
 </Router>
