4d71a6ee2c70efdc2442457924d33039e3d71edb
maxamed1st <maxamed1st@icloud.com>
2023-07-05 18:04:16 +0100

    Add onClick prob + change var names for readability

--- src/lib/PlusBtn.svelte
+++ src/lib/PlusBtn.svelte
@@ -1,11 +1,11 @@
 <script>
   export let onClick;
   export let bg = "bg-primary";
-  export let bgFocus = "bg-primary-focus";
+  export let hover = "bg-primary-focus";
 </script>
 <button
   on:click={onClick}
-  class= {`rounded-full w-14 h-14 self-end mr-3 fixed bottom-3 text-5xl text-base-100 hover:text-white ${bg} hover:${bgFocus}`}
+  class= {`rounded-full w-14 h-14 self-end mr-3 fixed bottom-3 text-5xl text-base-100 hover:text-white ${bg} hover:${hover}`}
 >
   +
 </button>
--- src/pages/Folders.svelte
+++ src/pages/Folders.svelte
@@ -44,5 +44,5 @@
       {/if}
     {/each}
   {/if}
-  <PlusBtn />
+  <PlusBtn onClick={e => showModal = true}/>
 </main>
