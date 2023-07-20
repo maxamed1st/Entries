fd1a568084e326b62f9310f73e63062adcf23992
maxamed1st <maxamed1st@icloud.com>
2023-07-20 19:21:35 +0100

    update modal: improve design and functionality

--- src/features/folders/CreateFolder.svelte
+++ src/features/folders/CreateFolder.svelte
@@ -1,28 +1,49 @@
 <script>
   import { currentFolder, userCol } from "../../lib/store";
+  import { navigate } from "svelte-routing";
+
   export let showModal;
-  let dialog;
+  let modal;
   let title;
-  $: if(dialog && showModal) dialog.showModal();
+
   function createFolder() {
+    //make sure form is not empty
+    title = title.trim();
+    if (!title.length) return (showModal = false);
+    //Set currentFolder for redirection
     currentFolder.set(title);
+    //add to db
     userCol.createFolder($currentFolder);
+    //reset form and close modal
+    title = "";
     showModal = false;
-    dialog.close();
+    //redirect to the new folder
+    navigate("/entries");
   }
 </script>
-{#if showModal}
-  <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <dialog 
-  bind:this = {dialog} 
-  on:close = {() => showModal = false}
-  on:click|self = {dialog.close()}
-  class="flex flex-col gap-1"
+
+<dialog bind:this={modal} open={showModal} class="modal">
+  <form
+    method="dialog"
+    on:submit|preventDefault={createFolder}
+    class="modal-box"
   >
-    <form on:submit|preventDefault={createFolder}>
-      <input type=title bind:value={title} required 
-      class="border" />
-      <button type="submit"> create </button>
-    </form>
-  </dialog>
-{/if}
+    <button
+      type="button"
+      on:click={() => (showModal = false)}
+      class="btn btn-sm btn-circle btn-ghost absolute top-2 right-2">✕</button
+    >
+    <label for="title" class="label"> New Folder </label>
+    <input
+      type="title"
+      id="title"
+      bind:value={title}
+      placeholder="name"
+      class="input bg-neutral text-neutral-content w-full"
+      required
+    />
+    <button type="submit" class="btn btn-primary flex mt-6 ml-auto"
+      >create</button
+    >
+  </form>
+</dialog>
